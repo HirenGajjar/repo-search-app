ae8113a0219ac4742cdc5691a4abb6cb3697bfa1
HirenGajjar <gajjarhiren111@gmail.com>
2024-12-12 15:11:22 -0800

    Add sidebar component

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import HomePage from "./Pages/HomePage";
+import LoginPage from "./Pages/LoginPage";
+import SignupPage from "./Pages/SignupPage";
+import ExplorePage from "./Pages/ExplorePage";
+import LikesPage from "./Pages/LikesPage";
+import Sidebar from "./Components/Sidebar";
+import { Route, Routes } from "react-router-dom";
+function App() {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <div className="max-w-5xl my-5 text-white mx-auto transition-all duration-300 flex-1">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/explore" element={<ExplorePage />} />
+          <Route path="/likes" element={<LikesPage />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
